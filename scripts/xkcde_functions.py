fea253e1003ed7be888e7836d44eb28a1e146339
Shivam Bajpayi <awes0menessInc@users.noreply.github.com>
2016-09-18 05:59:22 -0400

    refactor+device-log

--- scripts/xkcde_functions.py
+++ scripts/xkcde_functions.py
@@ -5,27 +5,8 @@ functions.
 from gpiozero import LED
 from time import sleep
 
-def intrepreter(text):
-    """ Manipulates and searches for keywords to different functions."""
 
-    # Separating keywords using the default demlimiter.
-    keywords = text.split(" ")
-
-    # Creating keyword lists for different functions.
-    listLED=['light','LED','bright','lamp','torch','glare','glow','dazzle']
-    listMusic=['music','song','play','artist','tune','track']
-
-    for keyword in keywords:
-        if keyword in listLED:
-            functions_LED()
-            break
-        elif keyword in listMusic:
-            functions_Music()
-            break
-        else:
-            continue
-
-def functions_LED():
+def functions_LED(keywords):
     """ Judges which function to use."""
     # Hard-coded coloured LED lights positions.
     green1 = LED(14)
@@ -34,40 +15,42 @@ def functions_LED():
     green2 = LED(24)
     red2 = LED(25)
     blue2 = LED(8)
-
+    default_vals={'red':'off','green':'off','blue':'off'}
     for keyword in keywords:
         if keyword=="on" or keyword="up":
             if "red" in keywords:
                 red1.on()
                 red2.on()
-                break
+                default_vals['red']='on'
             elif "green" in keywords:
                 green1.on()
                 green.on()
-                break
+                default_vals['green']='on'
             else:
                 blue1.on()
                 blue2.on()
-                break
+                default_vals['blue']='on'
 
         elif keyword=="off":
            if "red" in keywords:
                 red1.on()
                 red2.on()
-                break
+                default_vals['red']='off'
             elif "green" in keywords:
                 green1.on()
                 green.on()
-                break
+                default_vals['red']='off'
             else:
                 blue1.on()
                 blue2.on()
-                break
-
+                default_vals['red']='off'
         else:
             continue
-        
 
-def functions_Music():
+    return default_vals
+
+
+def functions_Music(keywords, emotion):
     """ Judges which music function to use."""
     print("Something with Music")
+    return 2
